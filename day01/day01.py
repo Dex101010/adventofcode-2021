989dbaaa4d74bb5045faf4e4592f07115a408fdd
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-01 13:01:14 +0000

    Update day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -1,2 +1,6 @@
-x=open("input.txt","r").readlines()
+x=[int(x) for x in open("input.txt").readlines()]
 print(sum([int(k)<int(v) for k,v in zip(x,x[1:])]))
+
+#part two
+q=[x+y+z for x,y,z in zip(x,x[1:],x[2:])]
+print(sum([int(k)<int(v) for k,v in zip(q,q[1:])]))
