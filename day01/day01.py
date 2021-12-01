4fc03aea9481086b2205e198dc6a5b3fca566b9c
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-01 13:05:19 +0000

    golfers gotta golf

--- day01/day01.py
+++ day01/day01.py
@@ -1,6 +1,6 @@
-x=[int(x) for x in open("input.txt").readlines()]
-print(sum([int(k)<int(v) for k,v in zip(x,x[1:])]))
+a=lambda b:sum([k<v for k,v in zip(b,b[1:])])
 
-#part two
-q=[x+y+z for x,y,z in zip(x,x[1:],x[2:])]
-print(sum([int(k)<int(v) for k,v in zip(q,q[1:])]))
+x=[int(l) for l in open("input.txt").readlines()]
+
+z=[p+q+r for p,q,r in zip(x,x[1:],x[2:])]
+print(a(x),a(z))
