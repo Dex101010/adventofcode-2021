32d7b96a6d65b4042fee513b90b596264304c5f8
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-01 15:16:55 +0000

    Update day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -1,3 +1,17 @@
 a=lambda b,c:sum([k<v for k,v in zip(b,b[c:])])
 x=list(map(int,open("input.txt").readlines()))
 print(a(x,1),a(x,3))
+
+#deobfuscated below
+def calculate_function(list,offset):
+    sum=0
+    for left,right in zip(list,list[offset:]):
+        if left<right:
+            sum+=1
+    return sum
+
+with open("input.txt") as f:
+    listOfValues=[]
+    for line in f.readlines():
+        listOfValues.append(int(line))
+print(calculate_function(listOfValues,1),calculate_function(listOfValues,3))
