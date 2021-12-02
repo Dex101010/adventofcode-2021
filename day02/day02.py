50b15ce08b2210eceb5a82eb4121558fa7371e6c
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-02 18:47:02 +0000

    Update day02.py

--- day02/day02.py
+++ day02/day02.py
@@ -1,7 +1,6 @@
 a=lambda l,v:sum([[q,0][k!=v]for (k,q) in l])
-f=[(i[0],int(i[-2])) for i in open("input.txt").readlines()]
-print((a(f,"d")-a(f,"u"))*a(f,"f"))
-x,y,d=0,0,0
+f=[(i[0],int(i[-2])) for i in open("input.txt")]
+y,d=0,0
 for (k,q) in f:
-    x,y,d=[(x+q,y+q*d,d),(x,y,d+q*[1,-1][k=="u"])][k!="f"]
-print(x*y)
+    y,d=[(y+q*d,d),(y,d+q*[1,-1][k=="u"])][k!="f"]
+print((a(f,"d")-a(f,"u"))*a(f,"f"),a(f,"f")*y)
