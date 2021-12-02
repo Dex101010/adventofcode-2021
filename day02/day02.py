0169df81fb3449310c746841f48a68e80aff2dc5
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-02 19:34:10 +0000

    Update day02.py

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,11 @@
 f=[(i[0],int(i[-2])) for i in open("input.txt")]
 h,y,d,a=[0]*4
 for (k,q) in f:
-    h,y,d,a=[(h+q,y+q*a,d,a),[(h,y,d+q,a+q),(h,y,d-q,a-q)][k=="u"]][k!="f"]
+    h,y,d,a={"u":(h,y,d-q,a-q),"d":(h,y,d+q,a+q),"f":(h+q,y+q*a,d,a)}[k]
 print(h*d,y*h)
+
+
+#h is horizontal distance
+#d is depth used in p1
+#y is the depth for p2
+#a is the aim for p2
