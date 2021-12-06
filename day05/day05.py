ac98b40cb0806f39a6fdab8598edfd0e3e027008
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-06 05:30:33 +0000

    Update day05.py

--- day05/day05.py
+++ day05/day05.py
@@ -1,10 +1,6 @@
-from collections import defaultdict
-input=[[map(int,j.split(","))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
-points=defaultdict(int)
-
-my_func = lambda l,x,y,c,d: l+[(x,y)] if x==c and y==d else my_func(l+[(x,y)],[[x+1,x-1][x>c],x][x==c],[[y+1,y-1][y>d],y][y==d],c,d)
-ret=[my_func([],*a,*b) for a,b in input]
-for item in ret:
-    for thing in item:
-        points[thing]+=1
-print(len([1 for point in points if points[point]>1]))
+t=[[map(int,j.split(","))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
+q={}
+f = lambda l,x,y,c,d: l+[(x,y)] if x==c and y==d else f(l+[(x,y)],[[x+1,x-1][x>c],x][x==c],[[y+1,y-1][y>d],y][y==d],c,d)
+for i in sum(map(lambda x:f([],*x[0],*x[1]),t),[]):
+    q[i]=q[i]+1 if i in q else 1
+print(len([1 for p in q if q[p]>1]))
