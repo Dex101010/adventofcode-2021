fa8b7dab68b4e7d41359bcd8aa170992838d9dad
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-06 04:18:39 +0000

    Update day05.py

--- day05/day05.py
+++ day05/day05.py
@@ -1,14 +1,11 @@
 from collections import defaultdict
-import time
-start_time = time.time()
 input=[[list(map(int,j.split(",")))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
 points=defaultdict(int)
-for a,b in input:
-    (x,y)=a
+for (a,b),(c,d) in input:
+    x,y=a,b
     points[(x,y)]+=1
-    while not(x==b[0] and y==b[1]):
-        y+=0 if a[1]==b[1] else [1,-1][a[1]>b[1]]
-        x+=0 if a[0]==b[0] else [1,-1][a[0]>b[0]]
+    while x!=c or y!=d:
+        if b!=d:y+=[1,-1][b>d]
+        if a!=c:x+=[1,-1][a>c]
         points[(x,y)]+=1
 print(len([1 for point in points if points[point]>1]))
-print("--- %s seconds ---" % (time.time() - start_time))
