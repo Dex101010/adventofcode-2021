34a50ce8f6e075fcade09e3f407f5867a6ca31bb
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-06 05:03:16 +0000

    Update day05.py

--- day05/day05.py
+++ day05/day05.py
@@ -1,11 +1,10 @@
 from collections import defaultdict
-input=[[list(map(int,j.split(",")))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
+input=[[map(int,j.split(","))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
 points=defaultdict(int)
-for (a,b),(c,d) in input:
-    x,y=a,b
-    points[(x,y)]+=1
-    while x!=c or y!=d:
-        if b!=d:y+=[1,-1][b>d]
-        if a!=c:x+=[1,-1][a>c]
-        points[(x,y)]+=1
+
+my_func = lambda l,x,y,c,d: l+[(x,y)] if x==c and y==d else my_func(l+[(x,y)],[[x+1,x-1][x>c],x][x==c],[[y+1,y-1][y>d],y][y==d],c,d)
+ret=[my_func([],*a,*b) for a,b in input]
+for item in ret:
+    for thing in item:
+        points[thing]+=1
 print(len([1 for point in points if points[point]>1]))
