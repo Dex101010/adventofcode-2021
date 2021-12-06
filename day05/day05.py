bd3f8f0bd9f11bdf3b2b38b9edf9b46fe6e4c754
Dex101010 <39387561+Dex101010@users.noreply.github.com>
2021-12-06 04:02:24 +0000

    Update day05.py

--- day05/day05.py
+++ day05/day05.py
@@ -1,30 +1,14 @@
 from collections import defaultdict
 import time
 start_time = time.time()
-
-with open("input.txt","r") as f:
-    input = f.readlines()
-input=[[list(map(int,j.split(",")))for j in i[:-1].split(" -> ")] for i in input]
-
-
-def get_points_between(a,b):
-    ret=[]
-    if a[0]==b[0]:
-        for y in range(min(a[1],b[1]),max(a[1],b[1])+1):
-            ret.append((a[0],y))
-    elif a[1]==b[1]:
-        for x in range(min(a[0],b[0]),max(a[0],b[0])+1):
-            ret.append((x,a[1]))
-    else:
-        pass
-    return ret
-
+input=[[list(map(int,j.split(",")))for j in i[:-1].split(" -> ")] for i in open("input.txt")]
 points=defaultdict(int)
-count = 0
-for pair in input:
-    for point in get_points_between(pair[0],pair[1]):
-        points[point]+=1
-        if points[point]==2:
-            count+=1
-print(count)
+for a,b in input:
+    (x,y)=a
+    points[(x,y)]+=1
+    while not(x==b[0] and y==b[1]):
+        y+=0 if a[1]==b[1] else [1,-1][a[1]>b[1]]
+        x+=0 if a[0]==b[0] else [1,-1][a[0]>b[0]]
+        points[(x,y)]+=1
+print(len([1 for point in points if points[point]>1]))
 print("--- %s seconds ---" % (time.time() - start_time))
